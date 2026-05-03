597850f9087aa7e9109366ae87b338fb5be2e4e9
KayyyRueee <khutchinson22@ivytech.edu>
2026-05-03 06:07:27 -0400

    Update today_string.py

--- today_string.py
+++ today_string.py
@@ -1,4 +1,9 @@
-with open("today.txt", "r") as file:
+with open("today.txt" , "r") as file:
     today_string = file.read()
 
 print(today_string)
+
+from datetime import datetime
+
+date_object = datetime.strptime(today_string.strip(), "%B %d, %Y")
+print(date_object)
